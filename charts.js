90fc5e7df8fff75f4a7e6d9768d9fa5445175f11
Micah Lund <micah.r.lund@gmail.com>
2020-02-03 11:26:08 -0700

    Added title to chart and created layout and displays in the data object.

--- charts.js
+++ charts.js
@@ -1,5 +1,9 @@
 let myChart = document.getElementById("my-chart").getContext("2d");
 
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = "black";
+
 let jsChart = new Chart(myChart, {
   type: "pie", //bar, horizontal, pie, line, doughnut, radar, polarArea
   data: {
@@ -10,9 +14,36 @@ let jsChart = new Chart(myChart, {
         data: [20, 15, 15, 5, 45],
         backgroundColor: ["red", "blue", "green", "yellow", "orange"],
         borderWidth: 1,
-        borderColor: "#000"
+        borderColor: "gray",
+        hoverBorderWidth: 4,
+        hoverBorderColor: "black"
       }
     ]
   },
-  options: {}
+  options: {
+    title: {
+      display: true,
+      text: "Popular Colors",
+      fontSize: 25
+      //fontColor: "black"
+    },
+    legend: {
+      display: true,
+      //position: "right",
+      labels: {
+        fontColor: "gray"
+      }
+    },
+    layout: {
+      padding: {
+        left: 50,
+        right: 0,
+        bottom: 0,
+        top: 0
+      }
+    },
+    tooltips: {
+      enabled: true
+    }
+  }
 });
