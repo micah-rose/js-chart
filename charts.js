d9d096b8c18a49149ad0132e9502f033d3d0fb52
Micah Lund <micah.r.lund@gmail.com>
2020-02-03 11:04:33 -0700

    First commit, starter project for learning how to manipulate data using Chart.js

---
+++ charts.js
@@ -0,0 +1,15 @@
+let myChart = document.getElementById("my-chart").getContext("2d");
+
+let jsChart = new Chart(myChart, {
+  type: "pie", //bar, horizontal, pie, line, doughnut, radar, polarArea
+  data: {
+    labels: ["Red", "Blue", "Green", "Yellow", "Orange"],
+    datasets: [
+      {
+        label: "Colors",
+        data: [20, 15, 15, 5, 45]
+      }
+    ]
+  },
+  options: {}
+});
